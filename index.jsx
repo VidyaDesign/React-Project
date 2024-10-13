bac823e2348cd529dd426b77d6920d368370d096
Mahavidya <104791133+VidyaDesign@users.noreply.github.com>
2024-10-13 14:04:35 -0300

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,37 +1,11 @@
-import styles from './Header.module.css'
+import styles from './Footer.module.css'
 
-function Header() {
-  return (
-    <header className={styles.header}>
-      <div className={styles.box}>
-        <span>@mahavidya</span>
-      </div>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <a className={styles.nav_a} href='https://github.com/VidyaDesign'>
-              GitHub
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a className={styles.nav_a} href='https://vidyadesign.com.br'>
-              UX Design
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.nav_a}
-              href='https://www.figma.com/files/team/1324840912746010534/recents-and-sharing/recently-viewed?fuid=1287812351867930952'
-            >
-              Figma
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            ©2024 - Vidya Design
+        </footer>
+    )
 }
 
-export default Header
-
+export default Footer
