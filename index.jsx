c869b46e84a903f2de6ac7b4ca749b337e76957e
Mahavidya <104791133+VidyaDesign@users.noreply.github.com>
2024-10-12 22:59:13 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            ©2024 - Vidya Design
+        </footer>
+    )
+}
+
+export default Footer
